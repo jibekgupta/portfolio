8ad586cde135e74964220a3fb1a67182e70ba452
jibekgupta <jibek.gupta@bison.howard.edu>
2026-01-10 00:01:34 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
-#!/usr/bin/env bash
+set -o errexit
 
-# Upgrade pip
-pip install --upgrade pip
-
-# Install all dependencies from requirements.txt
 pip install -r requirements.txt
-
-# Collect static files for production
-python manage.py collectstatic --noinput
-
-# Apply database migrations
+python manage.py collectstatic --no-input
 python manage.py migrate
